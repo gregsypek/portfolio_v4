83826aac529044cbf40716f26e3226b998782554
Greg <47268138+gregsypek@users.noreply.github.com>
2024-11-20 10:04:41 +0100

    creating components

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,31 @@
-export default function App() {
-	return <h1>hello</h1>;
+import Nav from "./components/Nav";
+import About from "./sections/About";
+import Contact from "./sections/Contact";
+import Experience from "./sections/Experience";
+import Footer from "./sections/Footer";
+import Hero from "./sections/Hero";
+
+function App() {
+	return (
+		<main className="relative bg-primary-dark text-white">
+			<Nav />
+			<section className="xl:padding-1 wide:padding-r padding-b">
+				<Hero />
+			</section>
+			<section className="padding">
+				<About />
+			</section>
+			<section className="padding">
+				<Experience />
+			</section>
+			<section className="padding">
+				<Contact />
+			</section>
+			<section className="padding">
+				<Footer />
+			</section>
+		</main>
+	);
 }
+
+export default App;
