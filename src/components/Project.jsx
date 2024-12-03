23bc4a3f12237cb28e61e337d6cba19892ad43a6
Greg <47268138+gregsypek@users.noreply.github.com>
2024-12-03 17:51:32 +0100

    fix margins and paddings for projects

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -39,13 +39,11 @@ function Project({ project }) {
 		};
 	}, [handleKeyDown]);
 
-	console.log("🚀 ~ Project ~ project:", project);
-	console.log("🚀 ~ Project ~ title:", title);
 	return (
-		<>
-			<div className="flex flex-col  lg:flex-row lg:justify-between ">
-				<div className="flex justify-between lg:hidden my-2 ">
-					<h4 className="text-base md:text-xl text-blue-grey uppercase  lg:mx-0 font-bold">
+		<div className="  hero-gradient relative before:rotate-180   before:translate-y-[20%] sm:before:-translate-y-[20%] sm:before:translate-x-[30%]  sm:before:scale-125 before:opacity-50">
+			<div className="flex flex-col lg:flex-row lg:justify-between margin-y relative z-20">
+				<div className="flex justify-between lg:hidden my-2  ">
+					<h4 className="section-title text-blue-grey uppercase  lg:mx-0 font-bold">
 						{title}
 					</h4>
 					<a href={`${github}`} target="_blank" rel="noreferrer">
@@ -54,42 +52,68 @@ function Project({ project }) {
 						</span>
 					</a>
 				</div>
-				<ul className="my-2 md:my-5 lg:my-10 flex gap-2 lg:gap-3 text-xs md:text-base lg:text-lg font-bold  text-dark-grey  flex-wrap  lg:mx-0">
+				<ul className="my-2 md:my-5 lg:my-10 flex gap-2 lg:gap-3 text-xs md:text-base lg:text-lg font-bold  text-dark-grey  flex-wrap  lg:mx-0 uppercase">
 					{tags.length > 0 ? tags.map((tag) => <li key={tag}>{tag}</li>) : null}
 				</ul>
 				<div className="lg:flex gap-5 my-auto mx-auto lg:mx-0 hidden ">
 					<Button onClick={handleNext}>
-						<LuArrowLeftCircle size={32} stroke="#18457C" />
+						<div className="group">
+							<LuArrowLeftCircle
+								size={32}
+								stroke="#AEAEAE"
+								strokeWidth={1}
+								className="group-hover:stroke-[#e7e4e4]"
+							/>
+						</div>
 					</Button>
 					<Button onClick={handlePrev} disabled={isSingleImage}>
-						<LuArrowRightCircle size={32} stroke="#18457C" />
+						<div className="group">
+							<LuArrowRightCircle
+								size={32}
+								stroke="#AEAEAE"
+								strokeWidth={1}
+								className="group-hover:stroke-[#e7e4e4]"
+							/>
+						</div>
 					</Button>
 				</div>
 			</div>
-			<div className=" mt-3 md:mt-5 lg:mt-0 flex flex-col lg:grid flex-1 lg:grid-cols-[1fr_2fr]  gap-5 md:gap-5 b ">
+			<div className=" flex flex-col lg:grid flex-1 lg:grid-cols-[1fr_2fr] gap-5 relative z-20">
 				<div className="flex flex-col items-center lg:items-start gap-4 md:gap-5 lg:gap-10 md:py-5 lg:py-10 px-2 lg:px-8  border-tertiary-blue rounded-lg justify-between order-2 lg:order-none">
 					<div className="flex-1 flex flex-col gap-3 lg:gap-10 relative">
 						<div className="lg:flex justify-between hidden">
-							<h4 className="text-base md:text-xl text-light-grey uppercase mx-auto lg:mx-0 font-bold">
+							<h4 className="section-title text-light-grey uppercase mx-auto lg:mx-0 font-bold">
 								{title}
 							</h4>
 							<a href={`${github}`} target="_blank" rel="noreferrer">
 								<span>
-									<LuGithub size={32} stroke="#18457C" />
+									<LuGithub size={32} stroke="#18457C" strokeWidth="1" />
 								</span>
 							</a>
 						</div>
 						<div className="flex gap-5 my-auto mx-auto lg:mx-0 lg:hidden">
 							<Button onClick={handleNext}>
-								<LuArrowLeftCircle size={32} stroke="white" />
+								<div className="group">
+									<LuArrowLeftCircle
+										size={32}
+										stroke="#AEAEAE"
+										strokeWidth={1}
+										className="group-hover:stroke-[#e7e4e4]"
+									/>
+								</div>
 							</Button>
 							<Button onClick={handlePrev} disabled={isSingleImage}>
-								<LuArrowRightCircle size={32} stroke="#18457C" />
+								<div className="group">
+									<LuArrowRightCircle
+										size={32}
+										stroke="#AEAEAE"
+										strokeWidth={1}
+										className="group-hover:stroke-[#e7e4e4]"
+									/>
+								</div>
 							</Button>
 						</div>
-						<p className="text-gray text-xs md:text-base leading-normal">
-							{desc}
-						</p>
+						<p className="text-gray paragraph ">{desc}</p>
 					</div>
 					<div>
 						<Button type="red" to={live}>
@@ -97,7 +121,7 @@ function Project({ project }) {
 						</Button>
 					</div>
 				</div>
-				<div className="border-[2px] border-[#424345] rounded-xl p-1 md:p-5 order-1 lg:order-none h-[350px] sm:h-[450px] lg:h-[550px] ">
+				<div className="border-[2px]  border-pink-grey rounded-xl p-1 md:p-5 order-1 lg:order-none h-[350px] sm:h-[450px] lg:h-[550px] ">
 					<img
 						className="w-full hover:cursor-pointer h-full object-contain hover:scale-y-105 hover:scale-x-105 rounded-xl hover:rounded-xl transition-all  duration-350"
 						src={images[currentImageIndex]}
@@ -105,7 +129,7 @@ function Project({ project }) {
 					/>
 				</div>
 			</div>
-		</>
+		</div>
 	);
 }
 
--- src/sections/Projects.jsx
+++ src/sections/Projects.jsx
@@ -2,15 +2,13 @@ import { projects } from "../constants";
 import Project from "../components/Project";
 const Projects = () => {
 	return (
-		<div className="max-container h-full flex flex-col relative z-20">
-			<h1 className="section-header mx-auto lg:mx-0 mb-8">
-				My Websites & Apps
-			</h1>
+		<div className="max-container  h-full flex flex-col relative z-20">
+			<h1 className="section-header ">My Websites & Apps</h1>
 			{projects.map((project, index) => (
 				<>
 					<Project key={project.id} project={project} />
 					{index != projects.length - 1 && (
-						<hr className="my-8 lg:my-16 xl:opacity-0" />
+						<hr className="margin-y xl:opacity-0" />
 					)}
 				</>
 			))}
